b18af9e959b11ab9c2bcdd6c23bc82ee942219bf
aldimhernandez <aldana-hernandez@hotmail.com>
2022-04-10 00:34:23 -0300

    first test case created successfully

---
+++ cypress/integration/search.js
@@ -0,0 +1,21 @@
+/** Suitecase
+ * Git proyect manager link:https://github.com/aldimhernandez/clothing_shop_cypress_practice/projects/1
+ */
+
+describe('search elements', () => {
+
+    beforeEach(()=>{
+        cy.visit('/'); //get baseUrl from cypress.json
+    })
+//RE001
+    it('RE001: Search for elements with multiple results', () => {
+        cy.fixture('index').then( (index) => {
+            cy.get(index.searchBox).type('dress');
+            cy.get(index.searchBtn).click();
+        })
+
+        cy.fixture('searchResults').then((searchResult)=> {
+            cy.get(searchResult.title).should('contain', 'dress');
+        })
+    })
+})
